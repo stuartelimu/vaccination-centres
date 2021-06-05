fdb9e8f03333161bded2dc83249894e45a2c3353
stuartelimu <stuartelimu@gmail.com>
2021-06-05 04:04:24 +0300

    display centres on the map

--- centres/static/js/map.js
+++ centres/static/js/map.js
@@ -1,4 +1,7 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const map = L.map('map')
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
-map.fitWorld();
+const centres = JSON.parse(document.getElementById('centres-data').textContent);
+let feature = L.geoJSON(centres).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
+map.fitBounds(feature.getBounds(), { padding: [100, 100] });
+
--- centres/views.py
+++ centres/views.py
@@ -1,6 +1,16 @@
+import json
+
+from django.core.serializers import serialize
 from django.views.generic.base import TemplateView
+
+from .models import Centre
 
 
 class CentreMapView(TemplateView):
 
     template_name = 'map.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['centres'] = json.loads(serialize("geojson", Centre.objects.all()))
+        return context
