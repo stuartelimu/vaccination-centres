4a0e989e690a38736a0cd5504f9da9057dd2a113
stuartelimu <stuartelimu@gmail.com>
2021-06-05 12:56:17 +0300

    add allowed_hosts setting

--- src/settings.py
+++ src/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vaccination-centers.herokuapp.com']
 
 
 # Application definition
