1bc707d21f13eba57b96b08e322f339466adbb13
stuartelimu <stuartelimu@gmail.com>
2021-06-05 03:23:23 +0300

    delete sites app

--- sites/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'sites'
-
-urlpatterns = [
-    path('map/', views.SiteMapView.as_view()),
-]
--- sites/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class SiteMapView(TemplateView):
-
-    template_name = 'map.html'
