0c977ddcb285967462bbf420af7664a64478597b
stuartelimu <stuartelimu@gmail.com>
2021-06-05 12:50:59 +0300

    add static file setting

--- src/settings.py
+++ src/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static',
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
