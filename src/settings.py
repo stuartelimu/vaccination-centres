f3a03834f9465297c51beea2de0b1560ef2cec86
stuartelimu <stuartelimu@gmail.com>
2021-06-08 14:55:51 +0300

    add settings for static files

--- src/settings.py
+++ src/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
