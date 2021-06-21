555186e61869ab19d7c8298fea385678c53e590a
stuartelimu <stuartelimu@gmail.com>
2021-06-21 22:46:56 +0300

    create a model for test centres

--- centres/models.py
+++ centres/models.py
@@ -7,3 +7,15 @@ class Centre(models.Model):
     name = models.CharField(max_length=255)
     district = models.CharField(max_length=255)
     location = PointField()
+
+
+class TestCentre(models.Model):
+    '''A test centre with a name, street address, contact, fee and location'''
+    name = models.CharField(max_length=200)
+    street_address = models.CharField(max_length=200)
+    contact = models.CharField(max_length=10)
+    fee = models.FloatField(default=0.0)
+    location = PointField()
+
+    class Meta:
+        verbose_name = 'Test Centres'
